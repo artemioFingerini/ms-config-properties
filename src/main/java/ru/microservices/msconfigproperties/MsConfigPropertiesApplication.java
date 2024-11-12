d25eba4b9911ad8957a7863057d7b43c6c82d598
artemioFingerini <artemisanx@gmail.com>
2024-11-12 19:05:56 +0300

    Initial commit

---
+++ src/main/java/ru/microservices/msconfigproperties/MsConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package ru.microservices.msconfigproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigPropertiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/microservices/msconfigproperties/MsConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.microservices.msconfigproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
